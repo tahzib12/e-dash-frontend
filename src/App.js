8f589927e125b73af98eacdd4f1ee1010be0977c
tahzib12 <tzbqdr786@gmail.com>
2025-07-10 13:26:46 +0530

    Frontend initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import PrivateComponent from './components/PrivateComponent';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route element={<PrivateComponent /> } >
+          <Route path='/' element={<h1>Product listing component</h1>} />
+          <Route path='/product' element={<h1>Add Product listing component</h1>} />
+          <Route path='/update' element={<h1>Update Product listing component</h1>} />
+          <Route path='/logout' element={<h1>Logout Product listing component</h1>} />
+          <Route path='/profile' element={<h1>Profile Product listing component</h1>} />
+          </Route>
+          <Route path='/signup' element={ <SignUp />} />
+          <Route path='/login' element={ <Login />} />
+        </Routes> 
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
